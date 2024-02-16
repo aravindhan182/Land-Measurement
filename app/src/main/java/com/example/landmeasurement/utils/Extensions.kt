34aef39f64c3e814963fe104d6df7288fe41e25e
aravindh2106 <aravindhan@logicsoft.co.in>
2024-02-16 10:29:12 +0530

    conversion method changed

--- app/src/main/java/com/example/landmeasurement/utils/Extensions.kt
+++ app/src/main/java/com/example/landmeasurement/utils/Extensions.kt
@@ -1,11 +1,12 @@
 package com.example.landmeasurement.utils
 
+import java.math.BigDecimal
 import kotlin.math.pow
 import kotlin.math.round
 
 
 fun Double.scientificNotationToDecimal(): String {
-    return String.format("%.15f", this)
+    return BigDecimal(this).toPlainString()
 }
 fun roundToDecimalPlaces(value: Double, decimalPlaces: Int): Double {
     val multiplier = 10.0.pow(decimalPlaces)
